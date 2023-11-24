99f1785cd65c2fa74620974177792761f92239c7
Taylan YILDIZ <taylanyildz@outlook.com>
2023-11-24 17:02:47 +0300

    register activation page updated

--- src/features/activations/register-activaton/index.tsx
+++ src/features/activations/register-activaton/index.tsx
@@ -4,7 +4,7 @@ import ERouter from "@Routes/router_enum";
 import { useQuery } from "@Utils/functions";
 import { Box, CircularProgress, Grid, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, Navigate, useNavigate } from "react-router-dom";
 import SuccessBox from "@Components/SuccessBox";
 import VisibilityComp from "@Components/VisibilityComp";
 
@@ -37,10 +37,9 @@ function RegisterActivationPage() {
   /// Initialize component
   useEffect(() => {
     if (email && code) registerActivate();
-    console.log(`Email: ${email}\nCode: ${code}\n`);
-  }, [email, code]);
+  }, []);
 
-  // if (!email || !code) return <Navigate to={ERouter.Base} />;
+  if (!email || !code) return <Navigate to={ERouter.Base} />;
 
   return (
     <Box className="login-page">
