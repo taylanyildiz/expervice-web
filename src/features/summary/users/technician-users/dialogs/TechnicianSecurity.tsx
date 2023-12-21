7cee591920529d229d5e7a7e06d498484696f446
Taylan YILDIZ <taylanyildz@outlook.com>
2023-12-21 21:27:10 +0300

    users tab bar visibility condition updated for technician users

--- src/features/summary/users/technician-users/dialogs/TechnicianSecurity.tsx
+++ src/features/summary/users/technician-users/dialogs/TechnicianSecurity.tsx
@@ -1,3 +1,5 @@
+import DisabledComp from "@Components/DisabledComp";
+import { useAccount } from "@Features/summary/company/helper/company_helper";
 import TranslateHelper from "@Local/index";
 import TechnicianUser from "@Models/technician-user/technician_user";
 import {
@@ -11,45 +13,51 @@ import { FormikProps } from "formik";
 
 function TechnicianSecurity(props: { formik: FormikProps<TechnicianUser> }) {
   const { formik } = props;
+
+  /// Account store
+  const { isInternal, isOwner } = useAccount();
+
   return (
-    <Grid container>
-      <Grid item xs={12}>
-        <Box>
-          <Grid container>
-            <Grid item flexGrow={1}>
-              <Typography
-                variant="h1"
-                fontSize={15}
-                children={TranslateHelper.securityLogin()}
-              />
+    <DisabledComp disabled={!(isInternal || isOwner)}>
+      <Grid container>
+        <Grid item xs={12}>
+          <Box>
+            <Grid container>
+              <Grid item flexGrow={1}>
+                <Typography
+                  variant="h1"
+                  fontSize={15}
+                  children={TranslateHelper.securityLogin()}
+                />
+              </Grid>
             </Grid>
-          </Grid>
-        </Box>
+          </Box>
+        </Grid>
+        <Grid item xs={12} mt={2}>
+          <Typography
+            variant="h1"
+            fontSize={13}
+            children={TranslateHelper.userStatus()}
+          />
+        </Grid>
+        <Grid item>
+          <FormControlLabel
+            checked={formik.values.is_active}
+            onChange={() => formik.setFieldValue("is_active", true)}
+            control={<Checkbox size="small" />}
+            label={TranslateHelper.active()}
+          />
+        </Grid>
+        <Grid item>
+          <FormControlLabel
+            checked={!formik.values.is_active}
+            onChange={() => formik.setFieldValue("is_active", false)}
+            control={<Checkbox size="small" />}
+            label={TranslateHelper.inactive()}
+          />
+        </Grid>
       </Grid>
-      <Grid item xs={12} mt={2}>
-        <Typography
-          variant="h1"
-          fontSize={13}
-          children={TranslateHelper.userStatus()}
-        />
-      </Grid>
-      <Grid item>
-        <FormControlLabel
-          checked={formik.values.is_active}
-          onChange={() => formik.setFieldValue("is_active", true)}
-          control={<Checkbox size="small" />}
-          label={TranslateHelper.active()}
-        />
-      </Grid>
-      <Grid item>
-        <FormControlLabel
-          checked={!formik.values.is_active}
-          onChange={() => formik.setFieldValue("is_active", false)}
-          control={<Checkbox size="small" />}
-          label={TranslateHelper.inactive()}
-        />
-      </Grid>
-    </Grid>
+    </DisabledComp>
   );
 }
 
